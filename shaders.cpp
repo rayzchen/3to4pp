aeef609c882b53c37087b0a1c157ab1fcb8009d3
Ray Chen <tankimarshal2@gmail.com>
2024-05-29 16:39:45 +0100

    Add normal mapping code (disabled by macro)

--- shaders.cpp
+++ shaders.cpp
@@ -24,6 +24,7 @@ const char *Shaders::vertex = R"(
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in float aColIdx;
 out flat float colorIndex;
+out vec3 meshPos;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -33,6 +34,7 @@ void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     colorIndex = aColIdx;
+    meshPos = aPos;
 }
 )";
 
@@ -40,14 +42,18 @@ const char *Shaders::fragment = R"(
 #version 330 core
 #define MAX_TRIANGLES 36
 in flat float colorIndex;
+in vec3 meshPos;
 out vec4 FragColor;
 
-uniform mat4 model;
 uniform int border;
 uniform vec3 pieceColors[4];
+
+uniform mat4 model;
 uniform vec3[MAX_TRIANGLES] normals;
 uniform vec3 lightDir = vec3(-0.3f, -0.7f, -0.5f);
 uniform vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
+
+uniform int lighting = 1;
 
 void main()
 {
@@ -65,15 +71,44 @@ void main()
             objectColor = pieceColors[3];
         }
 
-        float ambientStrength = 0.8;
-        vec3 ambient = ambientStrength * lightColor;
+        if (lighting == 1) {
+#if defined(NORMAL_MAP)
+            vec2 texCoord;
+            vec3 tangent;
+            vec3 bitangent;
+            vec3 meshNormal = normals[gl_PrimitiveID];
+            vec3 surfacePos = meshPos - meshNormal;
+            if (abs(meshNormal.x) == 1.0f) {
+                texCoord = vec2(surfacePos.z * -meshNormal.x, surfacePos.y);
+                tangent = vec3(0, 0, -meshNormal.x);
+            } else if (abs(meshNormal.y) == 1.0f) {
+                texCoord = vec2(surfacePos.x, surfacePos.z * -meshNormal.y);
+                tangent = vec3(1, 0, 0);
+            } else if (abs(meshNormal.z) == 1.0f) {
+                texCoord = vec2(surfacePos.x * meshNormal.z, surfacePos.y);
+                tangent = vec3(meshNormal.z, 0, 0);
+            }
+            bitangent = cross(meshNormal, tangent);
+            texCoord = (texCoord + 1) / 2;
 
-        vec3 normal = mat3(model) * normals[gl_PrimitiveID];
-        float diff = max(dot(normal, normalize(-lightDir)), 0.0);
-        vec3 diffuse = diff * lightColor;
+            vec3 normalMap = normalize(vec3(0.1 - texCoord.x / 10, 0.1 - texCoord.y / 10, 1.0));
+            mat3 TBN = mat3(tangent, bitangent, meshNormal);
+            vec3 normal = mat3(model) * TBN * normalMap;
+#else
+            vec3 normal = mat3(model) * normals[gl_PrimitiveID];
+#endif
 
-        vec3 result = (ambient + diffuse * 0.5) * objectColor;
-        FragColor = vec4(result, 1.0);
+            float ambientStrength = 0.8;
+            vec3 ambient = ambientStrength * lightColor;
+
+            float diff = max(dot(normal, normalize(-lightDir)), 0.0);
+            vec3 diffuse = diff * lightColor;
+
+            vec3 result = (ambient + diffuse * 0.5) * objectColor;
+            FragColor = vec4(result, 1.0);
+        } else {
+            FragColor = vec4(objectColor, 1.0);
+        }
     }
 }
 )";
