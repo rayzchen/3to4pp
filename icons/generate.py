f154646f4185e0d35111ab86d2737040a4cadc48
Ray Chen <tankimarshal2@gmail.com>
2024-05-30 18:53:56 +0100

    Remove empty background space around icon

--- icons/generate.py
+++ icons/generate.py
@@ -1,6 +1,12 @@
 from PIL import Image
+import numpy as np
 
-im = Image.open("puzzle.png")
+im = Image.open("puzzle.png").convert('RGBA')
+data = np.array(im)
+mask = np.all(data[:,:,:3] == [51, 51, 51], axis = -1)
+data[mask] = [255, 255, 255, 0]
+im = Image.fromarray(data)
+
 sizes = [16, 32, 48, 64, 128]
 icons = []
 for size in sizes:
